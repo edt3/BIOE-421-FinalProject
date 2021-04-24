2573b0819155d7c00c1740485014d889619990ae
Eric T <ericdtorres@hotmail.com>
2021-04-23 23:45:15 -0500

    commented out scale.is_ready()

--- PythonPlotting/PythonPlotting/PythonPlotting.ino
+++ PythonPlotting/PythonPlotting/PythonPlotting.ino
@@ -3,13 +3,18 @@
 
 HX711 scale;
 
-const int LOADCELL_DOUT_PIN = 2;
-const int LOADCELL_SCK_PIN = 3;
+const int LOADCELL_DOUT_PIN = 3;
+const int LOADCELL_SCK_PIN = 2;
+
+//#define DOUT 
+#define calibration_factor 10000
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+scale.set_scale(10000);
+scale.tare();
 
 }
 
@@ -17,13 +22,14 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 
-  if (scale.is_ready()) {
-    long reading = scale.read();
+//  if (scale.is_ready()) {
+    float reading = scale.get_units();
 //    Serial.print("HX711 reading: ");
-    Serial.println(String(reading));
-  } else {
-    Serial.println("HX711 not found.");
-  }
+    Serial.println(reading);
+//    Serial.println(String(69));
+//  } else {
+//    Serial.println("HX711 not found.");
+//  }
 
   delay(1000);
   
